e198f99f07d3f4f00ed7664c964e8390c824930b
lucasadlerstein <lucasadlerstein@gmail.com>
2020-07-16 20:52:10 -0300

    deployment final

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     env: {
-        backendURL: 'http://send.adlerstein.com.ar/b',
-        frontendURL: 'https://adlersend.vercel.app/'
+        backendURL: 'https://send.adlerstein.com.ar',
+        frontendURL: 'https://adlersend.vercel.app',
+
+        backendURL_test: 'http://localhost:4000',
+        frontendURL_test: 'http://localhost:3000'
     }
 }
