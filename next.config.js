ba317a7b654a5855d3289052edf71bc8ba8da70b
lucasadlerstein <lucasadlerstein@gmail.com>
2020-07-17 17:19:22 -0300

    http server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        backendURL: 'https://send.adlerstein.com.ar',
+        backendURL: 'http://send.adlerstein.com.ar',
         frontendURL: 'https://adlersend.vercel.app',
 
         backendURL_test: 'http://localhost:4000',
