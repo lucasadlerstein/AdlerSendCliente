febfd1d7a560baa6c686d51678aff10153b48023
lucasadlerstein <lucasadlerstein@gmail.com>
2020-07-17 14:50:28 -0300

    getServerSide

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -4,7 +4,7 @@ import Layout from '../../components/Layout';
 import clienteAxios from '../../config/axios';
 import appContext from '../../context/app/appContext';
 
-export async function getStaticProps({params}) {
+export async function getServerSideProps({params}) {
     const {enlace} = params;
     const resultado = await clienteAxios.get(`/api/enlaces/${enlace}`);
 
@@ -16,7 +16,7 @@ export async function getStaticProps({params}) {
 }
 
 // Generar un enlace por cada slug
-export async function getStaticPaths() {
+export async function getServerSidePaths() {
     const enlaces = await clienteAxios.get('/api/enlaces');
 
     return {
