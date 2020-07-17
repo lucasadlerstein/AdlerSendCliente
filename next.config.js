14d25b7568866d96e97ae3e3f5d2d2dbfd52c108
lucasadlerstein <lucasadlerstein@gmail.com>
2020-07-17 19:14:32 -0300

    setting final domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         backendURL: 'https://send.adlerstein.com.ar',
-        frontendURL: 'https://adlersend.vercel.app',
+        frontendURL: 'https://s.adlerstein.com.ar',
 
         backendURL_test: 'http://localhost:4000',
         frontendURL_test: 'http://localhost:3000'
